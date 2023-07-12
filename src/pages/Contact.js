783edb6ac7a8e364390a2d289e36942a092c7538
Cheryl <smilie_kay@yahoo.com>
2023-07-12 16:33:38 -0500

    used FormSubmit and got contact form working

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,133 +7,35 @@ import styles from "../styles/contact.module.css"
 const TITLE = "Contact Me | " + Config.SITE_TITLE;
 const DESC = "Contact Cheryl Miller.";
 const CANONICAL = Config.SITE_DOMAIN + "/";
-
-class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: "",
-      email: "",
-      message: "",
-      errors: {
-        name: "",
-        email: "",
-        message: "",
-      },
-    };
-  }
-
-  handleInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.validateForm()) {
-      // Submit the form data
-      console.log("Form submitted successfully!");
-      // Reset the form fields
-      this.setState({
-        name: "",
-        email: "",
-        message: "",
-        errors: {
-          name: "",
-          email: "",
-          message: "",
-        },
-      });
-    }
-  };
-
-  validateForm = () => {
-    let { name, email, message } = this.state;
-    let errors = {};
-    let isValid = true;
-
-    if (!name.trim()) {
-      errors.name = "Name is required";
-      isValid = false;
-    }
-
-    if (!email.trim()) {
-      errors.email = "Email is required";
-      isValid = false;
-    } else if (!this.validateEmail(email)) {
-      errors.email = "Invalid email address";
-      isValid = false;
-    }
-
-    if (!message.trim()) {
-      errors.message = "Message is required";
-      isValid = false;
-    }
-
-    this.setState({ errors });
-    return isValid;
-  };
-
-  validateEmail = (email) => {
-    // Simple email validation regex
-    const emailRegex = /^\S+@\S+\.\S+$/;
-    return emailRegex.test(email);
-  };
-
-  render() {
-    const { name, email, message, errors } = this.state;
-
-    return (
+ 
+class Contact extends React.Component{
+  render(){
+    return(
       <main>
-         <div className={styles['content-container']}>
-        <Helmet>
-          <title>{TITLE}</title>
-          <link rel="canonical" href={CANONICAL} />
-          <meta name="description" content={DESC} />
-        </Helmet>
-        <div className={styles["title-container"]}>
-        <h2>Contact Me</h2>
-        <p>If you would like to learn more about me and what I can do for your company feel free do contact me and arrange a meeting. </p>
-        </div>
-        <form onSubmit={this.handleSubmit} className={styles["form-container"]}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              onChange={this.handleInputChange}
-            />
-            {errors.name && <span>{errors.name}</span>}
+        <div className={styles['content-container']}>
+          <Helmet>
+            <title>{TITLE}</title>
+            <link rel="canonical" href={CANONICAL}/>
+            <meta name="dexcription" content={DESC}/>
+          </Helmet>
+          <div className={styles["title-container"]}>
+            <h2>Contact Me</h2>
+            <p>If you would like to learn more about me and what I can contribute to your company please contact me and arrange a meeting.</p>
           </div>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={this.handleInputChange}
-            />
-            {errors.email && <span>{errors.email}</span>}
-          </div>
-          <div>
-            <label htmlFor="message">Message:</label>
-            <textarea
-              id="message"
-              name="message"
-              value={message}
-              onChange={this.handleInputChange}
-            ></textarea>
-            {errors.message && <span>{errors.message}</span>}
-          </div>
-          <button type="submit">Submit</button>
-        </form>
+          <form className={styles["form-container"]} action="https://formsubmit.co/0bd2d5d0faa9f1ba29e29b49abfc932d" method="POST">
+        <input type="text" name="name" placeholder="Name" required/>
+        <input type="email" name="email" placeholder="Email Address" required/>
+        <input type="hidden" name="_subject" value="New submission!"/>
+        {/* <input type="hidden" name="_next" value="https://yourdomain.co/thanks.html"/> */}
+     <input type="text" name="message" placeholder="Your Message here " required/>
+   
+     <button type="submit">Send</button>
+</form>
         </div>
       </main>
     );
   }
+
 }
 
 export default Contact;
