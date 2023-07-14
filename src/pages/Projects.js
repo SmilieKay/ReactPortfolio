b79d9fd3e607a26b05e34d04204cf0ddcbac520d
Cheryl <smilie_kay@yahoo.com>
2023-07-14 02:36:19 -0500

    cleaned up

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,67 +13,7 @@ import weatherImage from '../images/Weather.png';
 const TITLE = "My Projects |" + Config.SITE_TITLE;
 const DESC = "Cheryl Miller's Projects.";
 const CANONICAL = Config.SITE_DOMAIN + "/";
-// class Projects extends React.Component {
-//   render() {
-//     return (
-//       <main>
-//            <Helmet>
-//                 <title>{TITLE}</title>
-//                 <link rel="canonical" href={CANONICAL}/>
-//                 <meta name="description" content= {DESC}/>
-//               </Helmet>
-//       <div id="Projects">
-//         <section className={styles["index-category"]}>
-//           <p className={styles["index-category-title"]}> My Projects</p>
-//           <div className={styles["index-category-box"]}>
-//   <a href="https://smiliekay.github.io/Tron-Themed-JavaScript-Quiz/" className={styles["index-category-link"]}>
-//     <div className={styles["dark-overlay"]}>
-//       <p className={styles["index-category-description"]}>This is a JavaScript quiz that I designed and built. It is timed and uses local storage to keep your scores. Try your hand at it and see if you can win the special BONUS high score! </p>
-//     </div>
-//     <img src={quizImage}  alt="Quiz " className={styles["index-category-image"]}/>
-//   </a>
-// </div>
 
-     
-
-//           <div className={styles["index-category-box"]}>
-//           <a href="https://smiliekay.github.io/03-Challenge/" className={styles["index-category-link"]} >
-//             <div className={styles["dark-overlay"]}>
-            
-//             <p className={styles["index-category-description"]}>This password generator allows you to answer a series of questions an will generate a secure password from your answers </p></div>
-//             <img src={passImage}  alt="Password generator " className={styles["index-category-image"]} />
-//           </a>
-//           </div>
-//           <a href="https://smiliekay.github.io/Blown-Away-weather/" className={styles["index-category-box"]}>
-//             <div className={styles["dark-overlay"]}></div>
-//             <img src={weatherImage}  alt="Weather app "/>
-//             <p>You can put in your city and get the 5 day forecast. It was my frist time using a api and a wonderful learning experience!</p>
-//           </a>
-          
-//           <a href="https://smiliekay.github.io/Daily-Planner/" className={styles["index-category-box"]}>
-//             <div className={styles["dark-overlay"]}></div>
-//             <img src={plannerImage}  alt="planner app "/>
-//             <p>I designed this planner for someone working 12 hour shifts and used 24 hour time but also for those that work night shift as well so I allowed for 24 hour scheduling.  </p>
-//           </a>
-        
-//           <a href="https://github.com/SmilieKay/BattleHawksTailgate" className={styles["index-category-box"]}>
-//             <div className={styles["dark-overlay"]}></div>
-//             <img src={tailgateImage}  alt="Tailgate planner"/>
-//             <p>This was a group project that I was a part of, fans of the St. Louis BattleHawks can use to help plan their tailgate parties for future games.</p>
-//           </a>
-//           <a href="https://github.com/SmilieKay/ParkQuest" className={styles["index-category-box"]}>
-//             <div className={styles["dark-overlay"]}></div>
-//             <img src={parksImage}  alt="Park Quest"/>
-//             <p>This was another group project that allows users to look for National parks in a selected state and to see the weather in the parks area.  </p>
-//           </a>
-//         </section>
-//       </div>
-//       </main>
-//     );
-//   }
-// }
-
-// export default Projects;
 
 class Projects extends React.Component {
   render() {
